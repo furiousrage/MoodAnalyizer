4ba17d7447ccece88a0f0b533e9482bbcb2ec593
furiousrage <62274466+furiousrage@users.noreply.github.com>
2020-04-07 19:53:40 +0530

    Update MoodAnalyserReflector.java

--- src/main/java/com/bridgelabz/JUNIT/MoodAnalyserReflector.java
+++ src/main/java/com/bridgelabz/JUNIT/MoodAnalyserReflector.java
@@ -6,51 +6,51 @@ import java.lang.reflect.InvocationTargetException;
 
 public class MoodAnalyserReflector {
 
-    public static Constructor<?> getConstructor(Class<?>... param) throws CheckingMoodException {
+    public static Constructor<?> getConstructor(Class<?>... param) throws CustomizedMoodException {
         try {
             Class<?> moodAnalyserClass = Class.forName("com.bridgelabz.JUNIT.ModeAnalyser");
             Constructor<?> constructor = moodAnalyserClass.getConstructor(param);
             return constructor;
         } catch (ClassNotFoundException e) {
-            throw new CheckingMoodException(CheckingMoodException.ExceptionType.CLASS_NOT_FOUND_ISSUE, "Issue with CLASS NOT FOUND");
+            throw new CustomizedMoodException(CustomizedMoodException.ExceptionType.CLASS_NOT_FOUND_ISSUE, "Issue with CLASS NOT FOUND");
         } catch (NoSuchMethodException e) {
-            throw new CheckingMoodException(CheckingMoodException.ExceptionType.NO_SUCH_METHOD, "Enter Proper method name");
+            throw new CustomizedMoodException(CustomizedMoodException.ExceptionType.NO_SUCH_METHOD, "Enter Proper method name");
         }
     }
-    public static Object createModeAnalysier(Constructor<?> constructor, Object ... message) throws CheckingMoodException {
+    public static Object createModeAnalysier(Constructor<?> constructor, Object ... message) throws CustomizedMoodException {
         try {
             Object moodObject = constructor.newInstance(message);
             return moodObject;
 
         } catch (InstantiationException e) {
-            throw new CheckingMoodException(CheckingMoodException.ExceptionType.OBJECT_CREATION_ISSUE, "Issue with object creation");
+            throw new CustomizedMoodException(CustomizedMoodException.ExceptionType.OBJECT_CREATION_ISSUE, "Issue with object creation");
         } catch (IllegalAccessException e) {
-            throw new CheckingMoodException(CheckingMoodException.ExceptionType.METHOD_INVOCATION_ISSUE, "Issue with data entered");
+            throw new CustomizedMoodException(CustomizedMoodException.ExceptionType.METHOD_INVOCATION_ISSUE, "Issue with data entered");
         } catch (InvocationTargetException e) {
-            throw new CheckingMoodException(CheckingMoodException.ExceptionType.METHOD_INVOCATION_ISSUE, "Issue with data entered");
+            throw new CustomizedMoodException(CustomizedMoodException.ExceptionType.METHOD_INVOCATION_ISSUE, "Issue with data entered");
         }
     }
 
-    public static Object invokeMethod (ModeAnalyser moodObject, String analyseMood) throws CheckingMoodException {
+    public static Object invokeMethod (ModeAnalyser moodObject, String analyseMood) throws CustomizedMoodException {
             try {
                 return moodObject.getClass().getMethod(analyseMood).invoke(moodObject);
             } catch (IllegalAccessException | InvocationTargetException e) {
-                throw new CheckingMoodException(CheckingMoodException.ExceptionType.METHOD_INVOCATION_ISSUE, "Issue with data entered");
+                throw new CustomizedMoodException(CustomizedMoodException.ExceptionType.METHOD_INVOCATION_ISSUE, "Issue with data entered");
             } catch (NoSuchMethodException e) {
-                throw new CheckingMoodException(CheckingMoodException.ExceptionType.NO_SUCH_METHOD, "Enter Proper method name");
+                throw new CustomizedMoodException(CustomizedMoodException.ExceptionType.NO_SUCH_METHOD, "Enter Proper method name");
             }
 
         }
         public static void setFieldValue (ModeAnalyser moodObject, String fieldName, String fieldValue) throws
-        CheckingMoodException {
+        CustomizedMoodException {
             try {
                 Field field = moodObject.getClass().getDeclaredField(fieldName);
                 field.setAccessible(true);
                 field.set(moodObject, fieldValue);
             } catch (IllegalAccessException e) {
-                throw new CheckingMoodException(CheckingMoodException.ExceptionType.NO_SUCH_FIELD, "ENTER Proper field name");
+                throw new CustomizedMoodException(CustomizedMoodException.ExceptionType.NO_SUCH_FIELD, "ENTER Proper field name");
             } catch (NoSuchFieldException e) {
-                throw new CheckingMoodException(CheckingMoodException.ExceptionType.METHOD_INVOCATION_ISSUE, "Issue With Data Entered");
+                throw new CustomizedMoodException(CustomizedMoodException.ExceptionType.METHOD_INVOCATION_ISSUE, "Issue With Data Entered");
 
             }
 
