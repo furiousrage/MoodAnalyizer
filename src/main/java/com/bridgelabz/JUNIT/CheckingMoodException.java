f8d7bd283f7423be1bf9c56c42a89a467bb11d24
furiousrage <62274466+furiousrage@users.noreply.github.com>
2020-04-07 19:49:26 +0530

    Update CheckingMoodException.java

--- src/main/java/com/bridgelabz/JUNIT/CheckingMoodException.java
+++ src/main/java/com/bridgelabz/JUNIT/CheckingMoodException.java
@@ -1,13 +1,13 @@
 package com.bridgelabz.JUNIT;
 
-public class CheckingMoodException extends Exception {
+public class CustomizedMoodException extends Exception {
     public enum ExceptionType{
         ENTERED_NULL, NO_SUCH_METHOD, METHOD_INVOCATION_ISSUE, ENTERED_EMPTY, NO_SUCH_FIELD, OBJECT_CREATION_ISSUE, CLASS_NOT_FOUND_ISSUE;
     }
 
     public ExceptionType type;
 
-     public CheckingMoodException(ExceptionType type, String message){
+     public CustomizedMoodException(ExceptionType type, String message){
          super(message);
          this.type=type;
      }
